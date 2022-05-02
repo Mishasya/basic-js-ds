c3c69008e11f68505ac6f4db6da9d69a08101606
Mishasya <Mishasya@bk.ru>
2022-05-02 18:48:50 +0300

    feat: add solution stack

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,23 +1,54 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 */
 class BinarySearchTree {
+  constructor() {
+    this.base = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  add(data) {
+    let newNode = new Node(data)
+    if(!this.base) {
+      this.base = newNode
+      return
+    }
 
+    let currentNode = this.base
+
+    while(currentNode) {
+      if(newNode.value < currentNode.value) {
+        if(!currentNode.left) {
+          currentNode.left = newNode
+          return
+        }
+        currentNode = currentNode.left
+      } else {
+        if(!currentNode.right) {
+          currentNode.right = newNode
+          return
+        }
+        currentNode = currentNode.right
+      }
+    }
+  }
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
